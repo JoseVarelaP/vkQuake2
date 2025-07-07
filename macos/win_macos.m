c20fe3da52b52dd0fe47cfa22e0a8bd7a248e60b
Jose Varela <jose@josevarela.net>
2025-07-07 17:11:10 -0600

    Use direct NSWindow scaling factor

--- macos/win_macos.m
+++ macos/win_macos.m
@@ -399,16 +399,9 @@ void CocoaDestroyWindow(void)
 const void *CocoaAddMetalView(void)
 {
 	CAMetalLayer* layer = [CAMetalLayer layer];
-	
-	// Trick: Set the view content scale to 2.
-	// I have NO idea if this is THE WAY to do it, but it seems to do the trick for now.
-	// I need to figure how if there's a way to get it from the monitor.
-	[layer setContentsScale:2];
+	[layer setContentsScale: [window backingScaleFactor] ];
 	
 	window.contentView.layer = layer;
 	window.contentView.wantsLayer = YES;
-	
-	//MetalView *mv = [[MetalView alloc] initWithFrame:[[window contentView] frame]];
-	//[[window contentView] addSubview:mv];
 	return (__bridge const void *)(layer);
 }
